5af8113afb42b4ab4e4fdc43f3d1eb11f0ed782c
Yogeshh <tummayogesh1433@gmail.com>
2025-02-23 11:33:54 +0530

    gsg

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
 // import Button from "./components/Button";
 "use client";
+import {Button } from "@headlessui/react"
 import StarsCanvas from "./components/Main/StarCanvas";
 export default function Home() {
   return (
     <>
-      <StarsCanvas />
+      {/* <StarsCanvas /> */}
+
+      <div className="absolute top-0 left-0 right-0 z-[30] flex justify-center items-center h-20">
+        <Button>Click Me</Button>
+      </div>
     </>
   );
 }
